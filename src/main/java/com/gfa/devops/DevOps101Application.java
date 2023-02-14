5a682b24ff470f58f0272f837911a7e8018f2fcb
Zdeněk Janák <janak@astronomie.cz>
2023-02-14 13:54:37 +0100

    Initial commit with Spring generated skeleton

---
+++ src/main/java/com/gfa/devops/DevOps101Application.java
@@ -0,0 +1,13 @@
+package com.gfa.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOps101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOps101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gfa/devops/DevOps101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfa.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOps101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
