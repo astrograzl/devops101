c817a2f56173c5ba57a55abc276bfd08c0b68cb8
Zdeněk Janák <zdenek@astronomie.cz>
2023-02-26 03:25:58 +0100

    Separated backend and frontend conceptualy independent images

---
+++ frontman/src/main/java/com/gfa/frontman/FrontmanApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.frontman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontmanApplication.class, args);
+    }
+
+}
---
+++ frontman/src/test/java/com/gfa/frontman/FrontmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfa.frontman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
